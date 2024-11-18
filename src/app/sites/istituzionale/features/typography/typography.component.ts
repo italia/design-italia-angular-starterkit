56ed4d9183c6e76da9523cdc425c8d6aff695eb7
valerio como <v.como@innovazione.gov.it>
2024-11-18 11:54:09 +0000

    chore: minor fix

--- src/app/sites/istituzionale/features/typography/typography.component.ts
+++ src/app/sites/istituzionale/features/typography/typography.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  template: `<h1>Tipografia</h1><br/>
+  template: `
+  <div class="container">
+    <h1>Tipografia</h1><br/>
     <h1>Hello h1</h1>
     <h2>Hello h2</h2>
     <h3>Hello h3</h3>
     <h4>Hello h4</h4>
     <h5>Hello h5</h5>
     <h6>Hello h6</h6>
-
+    
     <p>Esempio di testo <u>sottolineato</u>.</p>
     <p>Esempio di testo <mark>evidenziato</mark>.</p>
     <p>Esempio di testo <em>corsivo</em>.</p>
@@ -18,6 +20,7 @@ import { Component } from '@angular/core';
     <p>Esempio di testo <ins>aggiuntivo</ins>.</p>
     <p>Esempio di testo <del>cancellato</del> o <i>invalido</i>.</p>
     <p>Esempio di testo <code>monospace</code>.</p>
+  </div>
     `,
 })
-export default class TypographyComponent {}
+export default class TypographyComponent { }
