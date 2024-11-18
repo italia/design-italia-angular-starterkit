db3d4e97c6843684de6d843220b7529aaadd3a8d
valerio como <v.como@innovazione.gov.it>
2024-11-18 12:00:51 +0000

    feat: evidenza section

---
+++ src/app/sites/istituzionale/features/home/evidenza.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-evidenza',
+    templateUrl: 'evidenza.component.html'
+})
+export class EvidenzaComponent { }
--- src/app/sites/istituzionale/features/home/home.component.ts
+++ src/app/sites/istituzionale/features/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { EvidenzaComponent } from './evidenza.component';
 
 @Component({
   standalone: true,
   templateUrl: './home.component.html',
+  imports: [EvidenzaComponent]
 })
 export default class HomeComponent { }
