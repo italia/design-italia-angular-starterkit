e3fbbdb711ff2067f69a6c54a611c0f4e62e057c
valerio como <v.como@innovazione.gov.it>
2024-11-18 13:54:33 +0000

    feat: svg to icon

--- src/app/sites/istituzionale/features/home/calendario.component.ts
+++ src/app/sites/istituzionale/features/home/calendario.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { ItButtonDirective, ItIconComponent } from "design-angular-kit";
 
 @Component({
     standalone: true,
     selector: 'app-calendario',
-    templateUrl: './calendario.component.html'
+    templateUrl: './calendario.component.html',
+    imports: [ItIconComponent, ItButtonDirective]
 })
 export class CalendarioComponent { }
