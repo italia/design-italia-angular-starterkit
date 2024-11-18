81204087dd1f2b6b7604e374d1324350ad8c9c05
valerio como <v.como@innovazione.gov.it>
2024-11-18 12:05:34 +0000

    chore: head section

---
+++ src/app/sites/istituzionale/features/home/head-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-head-section',
+    templateUrl: 'head-section.component.html'
+})
+export class HeadSectionComponent { }
--- src/app/sites/istituzionale/features/home/home.component.ts
+++ src/app/sites/istituzionale/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { EvidenzaComponent } from './evidenza.component';
+import { HeadSectionComponent } from './head-section.component';
 
 @Component({
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [EvidenzaComponent]
+  imports: [HeadSectionComponent, EvidenzaComponent]
 })
 export default class HomeComponent { }
