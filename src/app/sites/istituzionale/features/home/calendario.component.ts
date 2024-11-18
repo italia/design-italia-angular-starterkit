f2ae96453c1f3edd4453e03fedbcf1377ab4d9c9
valerio como <v.como@innovazione.gov.it>
2024-11-18 12:08:24 +0000

    feat: calendario

---
+++ src/app/sites/istituzionale/features/home/calendario.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-calendario',
+    templateUrl: './calendario.component.html'
+})
+export class CalendarioComponent { }
--- src/app/sites/istituzionale/features/home/home.component.ts
+++ src/app/sites/istituzionale/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CalendarioComponent } from "./calendario.component";
 import { EvidenzaComponent } from './evidenza.component';
 import { HeadSectionComponent } from './head-section.component';
 
 @Component({
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [HeadSectionComponent, EvidenzaComponent]
+  imports: [HeadSectionComponent, CalendarioComponent, EvidenzaComponent, CalendarioComponent]
 })
 export default class HomeComponent { }
