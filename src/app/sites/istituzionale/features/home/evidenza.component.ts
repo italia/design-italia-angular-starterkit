f4855d95699c03d559d2e553709677c395e17e4c
valerio como <v.como@innovazione.gov.it>
2024-11-18 12:39:25 +0000

    feat: svg to icon in evidenza

--- src/app/sites/istituzionale/features/home/evidenza.component.ts
+++ src/app/sites/istituzionale/features/home/evidenza.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { ItIconComponent } from "design-angular-kit";
 
 @Component({
     standalone: true,
     selector: 'app-evidenza',
-    templateUrl: 'evidenza.component.html'
+    templateUrl: 'evidenza.component.html',
+    imports: [ItIconComponent]
 })
 export class EvidenzaComponent { }
