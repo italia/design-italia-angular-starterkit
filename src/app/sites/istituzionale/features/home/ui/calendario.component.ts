909222c946ba181ea42a89b73d64acc339a77857
valerio como <v.como@innovazione.gov.it>
2024-11-19 10:05:03 +0000

    fix: removed deps

--- src/app/sites/istituzionale/features/home/ui/calendario.component.ts
+++ src/app/sites/istituzionale/features/home/ui/calendario.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 import { addDays, format } from 'date-fns';
 import { it } from "date-fns/locale";
 import { ItButtonDirective, ItCardComponent, ItCarouselComponent, ItCarouselItemComponent, ItIconComponent } from "design-angular-kit";
-import { CarouselModule, OwlOptions } from "ngx-owl-carousel-o";
 
 const slidesStoreFactory = () => {
     return Array
@@ -22,36 +21,11 @@ const getThisMonth = () => format(new Date(), 'MMMM yyyy', { locale: it })
     standalone: true,
     selector: 'app-calendario',
     templateUrl: './calendario.component.html',
-    imports: [ItIconComponent, ItButtonDirective, CarouselModule, TitleCasePipe, ItCarouselComponent, ItCarouselItemComponent, ItCardComponent]
+    imports: [ItIconComponent, ItButtonDirective, TitleCasePipe, ItCarouselComponent, ItCarouselItemComponent, ItCardComponent]
 })
 export class CalendarioComponent {
 
     readonly month = getThisMonth();
 
     readonly slidesStore = slidesStoreFactory();
-
-    readonly customOptions: OwlOptions = {
-        loop: true,
-        mouseDrag: false,
-        touchDrag: false,
-        pullDrag: false,
-        dots: false,
-        navSpeed: 700,
-        navText: ['', ''],
-        responsive: {
-            0: {
-                items: 1
-            },
-            400: {
-                items: 2
-            },
-            740: {
-                items: 3
-            },
-            940: {
-                items: 4
-            }
-        },
-        nav: true
-    }
 }
